d60bc9a48ca30f1262f7516868034b9849dfb42c
Eliud2005 <utti232008@utvco.edu.mx>
2025-10-07 12:04:10 -0600

    Ya no gane recuperar el qr

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/w8h1d8f',  // la ruta que tu QR apunta
-        destination: '/',      // tu home
-        permanent: true,       // 301 permanente
-      },
-    ]
-  },
+  /* config options here */
 };
 
 export default nextConfig;
